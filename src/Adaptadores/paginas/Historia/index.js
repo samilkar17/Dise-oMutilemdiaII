f48185c5ab1cf129a708c67e6d1d05b2a66ceb5e
marlonc98 <mackiller1998@gmail.com>
2021-10-04 22:18:10 -0500

    avatar en historia

--- src/Adaptadores/paginas/Historia/index.js
+++ src/Adaptadores/paginas/Historia/index.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 import Button from '../../componentes/Button';
+import { selectgender } from "../../../Puertos/feactures/gender/genderSlices";
 import './index.css';
+import { useSelector } from 'react-redux';
 
 export default function Historia() {
+    const setGender = useSelector(selectgender);
     const history = useHistory();
     const [stage, setStage] = useState(0); //0 first text, 1 second text, 3 zoomed
-    const avatar = 1;
     const changeStage = () => {
         if (stage == 1) history.push('/presentacion');
         else setStage(stage + 1);
     }
-    return <div className="app introApp" style={{ backgroundImage: `url('/assets/backgrounds/bg-historia-${avatar}-${stage}.png')` }}>
+    return <div className="app introApp" style={{
+        backgroundImage: `url('/assets/backgrounds/bg-historia-${setGender}-${stage}.png')`, backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center'
+    }}>
         <Button right={5} bottom={5} text="Siguiente" onClick={changeStage} />
     </div>
 }
